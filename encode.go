bc5924bea506fc16b0f068dbc43ff7e48d2c61d9
Oleg Kovalov <iamolegkovalov@gmail.com>
2019-06-21 20:13:43 +0200

    add initial encode code

---
+++ encode.go
@@ -0,0 +1,51 @@
+package bencode
+
+import (
+	"bytes"
+	"io"
+)
+
+// Marshaler is the interface implemented by types that
+// can marshal themselves into valid Bencode.
+type Marshaler interface {
+	MarshalBencode() ([]byte, error)
+}
+
+// Marshal returns bencode encoding of v.
+func Marshal(v interface{}) ([]byte, error) {
+	var e encoder
+	if err := e.Marshal(v); err != nil {
+		return nil, err
+	}
+	return e.Bytes(), nil
+}
+
+// An Encoder writes Bencode values to an output stream.
+type Encoder struct {
+	w io.Writer
+	e encoder
+}
+
+// NewEncoder returns a new encoder that writes to w.
+func NewEncoder(w io.Writer) *Encoder {
+	return &Encoder{w: w}
+}
+
+// Encode writes the Bencode encoding of v to the stream.
+func (enc *Encoder) Encode(v interface{}) error {
+	enc.e.Reset()
+	if err := enc.e.Marshal(v); err != nil {
+		return err
+	}
+	_, err := enc.w.Write(enc.e.Bytes())
+	return err
+}
+
+// we can ignore every error result from bytes.Buffer 'cause it's nil
+type encoder struct {
+	bytes.Buffer
+}
+
+func (e *encoder) Marshal(v interface{}) error {
+	return nil
+}

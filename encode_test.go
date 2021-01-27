f5cfd71362714b7c3f05065f193a0416015fd62e
Oleg Kovalov <oleg@hey.com>
2021-01-27 18:19:08 +0100

    add encoder bench

--- encode_test.go
+++ encode_test.go
@@ -219,3 +219,42 @@ func testLoopMarshal(t *testing.T, tcs []marshalTestCase) {
 		}
 	}
 }
+
+var marshalBenchData = map[string]interface{}{
+	"announce": ("udp://tracker.publicbt.com:80/announce"),
+	"announce-list": []interface{}{
+		[]interface{}{("udp://tracker.publicbt.com:80/announce")},
+		[]interface{}{[]byte("udp://tracker.openbittorrent.com:80/announce")},
+		[]interface{}{
+			"udp://tracker.openbittorrent.com:80/announce",
+			"udp://tracker.openbittorrent.com:80/announce",
+		},
+	},
+	"comment": []byte("Debian CD from cdimage.debian.org"),
+	"info": map[string]interface{}{
+		"name":         []byte("debian-8.8.0-arm64-netinst.iso"),
+		"length":       170917888,
+		"piece length": 262144,
+	},
+}
+
+func Benchmark_Marshal(b *testing.B) {
+	b.ReportAllocs()
+	for n := 0; n < b.N; n++ {
+		_, err := Marshal(marshalBenchData)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func Benchmark_MarshalTo(b *testing.B) {
+	dst := make([]byte, 0, 1<<12)
+	b.ReportAllocs()
+	for n := 0; n < b.N; n++ {
+		_, err := MarshalTo(dst, marshalBenchData)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
